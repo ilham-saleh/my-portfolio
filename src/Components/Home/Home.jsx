27752e8aceff701b096ef8477cb40d4275090a5a
ilham saleh <ilham2saleh@gmail.com>
2023-11-22 22:04:08 +0000

    home page created and adjusted for both laptop and mobile screen

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import CV from '../../assets/Ilham_Saleh_CV.pdf'
+
+import { FaArrowDown } from "react-icons/fa6"
+import randomGuy2 from '../../assets/random-guy-2.jpg'
+import './Home.css'
 
 function Home() {
     return (
-        <div>Home</div>
+        <div className="section grid home container">
+            <img src={randomGuy2} alt="random-guy" className="home-img" />
+
+            <div className="home-content-container">
+                <div className="home-title">
+                    <h1>I'm Ilham Saleh</h1>
+                    <h1>Front-End Developer</h1>
+                </div>
+                <div className="home-content">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, beatae sapiente perspiciatis commodi molestiae odio nemo ea excepturi doloremque vel!</p>
+                </div>
+
+                <a href={CV} download>
+                    <button className="button">
+                        Download CV
+                        <span>
+                            <FaArrowDown className="arrow-down"/>
+                        </span>
+                    </button>
+                </a>
+            </div>
+
+            <div className="color-block"></div>
+        </div>
     )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,22 +4,19 @@ import links from "../../data";
 
 import './Navbar.css'
 
-import { FaHome } from "react-icons/fa";
-import { FaUser } from "react-icons/fa";
-import { FaFolderOpen } from "react-icons/fa";
-import { MdContactPhone } from "react-icons/md";
 import { IoMdMenu } from "react-icons/io";
 import { FaTimes } from "react-icons/fa";
 
 function Navbar() {
     const [showNav, setShowNav] = useState(false)
+    const [displayNone, setDisplayNone] = useState(false)
 
 
     return (
         <nav className="nav-container">
             <div className={showNav ? "nav-menu show-nav" : "nav-menu"} >
                 <ul className="nav-list">
-                  {links.map((link, id) => (
+                  {links.map((link) => (
                     <li key={link.id}> 
                         <NavLink className={(({isActive}) => isActive ? "nav-link active" : "nav-link")} to={link.to} onClick={() => setShowNav(!showNav)}>
                             {link.icon}
@@ -30,7 +27,10 @@ function Navbar() {
                 </ul>
             </div>
 
-            <div className="nav-toggle" onClick={() => setShowNav(!showNav)}>
+            <div className="nav-toggle" onClick={() => {
+                setShowNav(!showNav)
+                setDisplayNone(!displayNone)
+            }}>
                 {!showNav ? <IoMdMenu className="toggle-icon"/> : <FaTimes className="toggle-icon"/>}
             </div>
         </nav>
